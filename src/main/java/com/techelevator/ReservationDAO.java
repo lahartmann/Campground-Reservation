d4824223b602ac940f8a0ba36445b527d7fc65d3
Eric McKeegan <ericmckeegan@gmail.com>
2020-02-20 13:15:29 -0500

    getters and setters for classes

--- src/main/java/com/techelevator/ReservationDAO.java
+++ src/main/java/com/techelevator/ReservationDAO.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
-public class ReservationDAO {
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+
+
+@Override
+public Reservation createReservation(Reservation newReservation) {
+	
+	String sqlNewReservaton = "INSERT INTO reservation(dreservation_id, site_id, name, from_date, to_date, create_date)"
+			+ " VALUES(?, ?, ?, ?, ? ,?)";
+	
+	newReservation.setId(getNextReservationID() );
+	
+	jdbcTemplate.update(sqlNewReservation, newReservation.getId(), newReservation.getName());
+	
+	return newReservation;
+
+	}
 }
--- src/main/java/com/techelevator/Site.java
+++ src/main/java/com/techelevator/Site.java
@@ -1,5 +1,56 @@
 package com.techelevator;
 
 public class Site {
+	private int siteId;
+	private int campgroundID;
+	private int siteNumber;
+	private int	maxOccupancy;
+	private boolean accessible;
+	private int maxRvLength;
+	private boolean utilites;
+	public int getSiteId() {
+		return siteId;
+	}
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}
+	public int getCampgroundID() {
+		return campgroundID;
+	}
+	public void setCampgroundID(int campgroundID) {
+		this.campgroundID = campgroundID;
+	}
+	public int getSiteNumber() {
+		return siteNumber;
+	}
+	public void setSiteNumber(int siteNumber) {
+		this.siteNumber = siteNumber;
+	}
+	public int getMaxOccupancy() {
+		return maxOccupancy;
+	}
+	public void setMaxOccupancy(int maxOccupancy) {
+		this.maxOccupancy = maxOccupancy;
+	}
+	public boolean isAccessible() {
+		return accessible;
+	}
+	public void setAccessible(boolean accessible) {
+		this.accessible = accessible;
+	}
+	public int getMaxRvLength() {
+		return maxRvLength;
+	}
+	public void setMaxRvLength(int maxRvLength) {
+		this.maxRvLength = maxRvLength;
+	}
+	public boolean isUtilites() {
+		return utilites;
+	}
+	public void setUtilites(boolean utilites) {
+		this.utilites = utilites;
+	}
+	
+	
 
 }
